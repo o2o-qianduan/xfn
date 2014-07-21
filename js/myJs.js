09c34fffb8a3cd034b31dc83324b9345b8288f92
ELesGong <elesgong@qq.com>
2014-07-22 04:15:42 +0800

    咨询文章页做了 提问！首页那些加减按钮什么的是不是坏掉了？有点不忍直视的样子？

--- js/myJs.js
+++ js/myJs.js
@@ -640,3 +640,73 @@ function tabOptionHover6(tabOptionIndex){
 
  /***个人中心页面根据左边菜单点击在右边区域显示相应的内容*****june*****end***/
 
+/*************输入框默认显示文字***********eles****************start**************************/
+ function placeHolder(obj, span) {
+        if (!obj.getAttribute('placeholder')) return;
+        var imitateMode = span === true ? true : false;
+        var supportPlaceholder = 'placeholder' in document.createElement('input');
+        if (!supportPlaceholder) {
+            var defaultValue = obj.getAttribute('placeholder');
+            if (!imitateMode) {
+                obj.onfocus = function() {
+                    (obj.value == defaultValue) && (obj.value = '');
+                    obj.style.color = '';
+                }
+                obj.onblur = function() {
+                    if (obj.value == defaultValue) {
+                        obj.style.color = '';
+                    } else if (obj.value == '') {
+                        obj.value = defaultValue;
+                        obj.style.color = '#ACA899';
+                    }
+                }
+                obj.onblur();
+            } else {
+                var placeHolderCont = document.createTextNode(defaultValue);
+                var oWrapper = document.createElement('span');
+                oWrapper.style.cssText = 'position:absolute; color:#ACA899; display:inline-block; overflow:hidden;';
+                oWrapper.className = 'wrap-placeholder';
+                oWrapper.style.fontFamily = getStyle(obj, 'fontFamily');
+                oWrapper.style.fontSize = getStyle(obj, 'fontSize');
+                oWrapper.style.marginLeft = parseInt(getStyle(obj, 'marginLeft')) ? parseInt(getStyle(obj, 'marginLeft')) + 3 + 'px' : 3 + 'px';
+                oWrapper.style.marginTop = parseInt(getStyle(obj, 'marginTop')) ? getStyle(obj, 'marginTop') : 1 + 'px';
+                oWrapper.style.paddingLeft = getStyle(obj, 'paddingLeft');
+                oWrapper.style.width = obj.offsetWidth - parseInt(getStyle(obj, 'marginLeft')) + 'px';
+                oWrapper.style.height = obj.offsetHeight + 'px';
+                oWrapper.style.lineHeight = obj.nodeName.toLowerCase() == 'textarea' ? '' : obj.offsetHeight + 'px';
+                oWrapper.appendChild(placeHolderCont);
+                obj.parentNode.insertBefore(oWrapper, obj);
+                oWrapper.onclick = function() {
+                    obj.focus();
+                }
+                //绑定input或onpropertychange事件
+                if (typeof(obj.oninput) == 'object') {
+                    obj.addEventListener("input", changeHandler, false);
+                } else {
+                    obj.onpropertychange = changeHandler;
+                }
+
+                function changeHandler() {
+                    oWrapper.style.display = obj.value != '' ? 'none' : 'inline-block';
+                }
+                /**
+                 * @name getStyle
+                 * @class 获取样式
+                 * @param {Object} obj 要获取样式的对象
+                 * @param {String} styleName 要获取的样式名
+                 */
+
+                function getStyle(obj, styleName) {
+                    var oStyle = null;
+                    if (obj.currentStyle) oStyle = obj.currentStyle[styleName];
+                    else if (window.getComputedStyle) oStyle = window.getComputedStyle(obj, null)[styleName];
+                    return oStyle;
+                }
+            }
+        }
+    }
+
+    placeHolder(document.getElementById("loginname"), true)
+    placeHolder(document.getElementById("pwd"), true)
+    placeHolder(document.getElementById("ass_area"), true)
+/*************输入框默认显示文字***********eles****************end**************************/
